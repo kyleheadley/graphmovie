03e048d43cc213156b669f3f671f0e6658a055c6
Kyle Headley <kheadley@umd.edu>
2014-08-20 08:35:43 -0400

    added change states

--- graph.js
+++ graph.js
@@ -50,7 +50,12 @@ function makeElement(label) {
         id: label,
         size: { width: width, height: height },
         attrs: {
-            text: { text: label, 'font-size': letterSize, 'font-family': 'monospace' },
+            text: {
+                text: label,
+                'font-size': letterSize,
+                'font-family': 'monospace',
+                'transform': ''
+            },
             rect: {
                 width: width, height: height,
                 rx: 5, ry: 5,
@@ -77,10 +82,13 @@ function parseSimpleAdaptonView(text){
         graphinfo.states[i] = {
             title: "No name",
             elementStates: [],
-            linkStates: []
+            linkStates: [],
+            cstates: []
         }
-        state = graphinfo.states[i]; //shortcut
-        lines = states[i].split('\n');
+        var state = graphinfo.states[i]; //shortcut
+        var changestates = states[i].split('Change state: ');
+        //first entry is the main state
+        var lines = changestates[0].split('\n');
         state.title = lines.shift();
         for(j=0;j<lines.length;j++){
             tokens = lines[j].split(' ');
@@ -121,18 +129,69 @@ function parseSimpleAdaptonView(text){
                     state.elementStates[toNodeIndex] = 'active';
                 }
                 //set up the link
-                linkIndex = graphinfo.links.indexOf(fromNode+','+toNode);
+                linkIndex = graphinfo.links.indexOf(fromNode+' '+toNode);
                 if(linkIndex == -1){
-                    graphinfo.links.push(fromNode+','+toNode);
+                    graphinfo.links.push(fromNode+' '+toNode);
                     linkIndex = graphinfo.links.length-1;
                 }
                 state.linkStates[linkIndex] = linkState;
             }//end link parse
-        }//end line parse
-    }//end state parse
+        }//end main state line parse
+        //parse the rest of the change states
+        for(j=1;j<changestates.length;j++){
+            graphinfo.states[i].cstates[j-1] = {
+                title: "No name",
+                elementStates: [],
+                linkStates: [],
+            }
+            state = graphinfo.states[i].cstates[j-1]; //shortcut
+            lines = changestates[j].split('\n');
+            state.title = lines.shift();
+            for(k=0;k<lines.length;k++){
+                tokens = lines[k].split(' ');
+                //process line as element
+                if(tokens.length == 2){
+                    ename = tokens[0];
+                    estate = tokens[1];
+                    eindex = graphinfo.elements.indexOf(ename);
+                    if(eindex == -1){
+                        graphinfo.elements.push(ename);
+                        eindex = graphinfo.elements.length-1;
+                    }
+                    state.elementStates.push({i: eindex,s: estate});
+                }//end element parse
+                //process line as link
+                if(tokens.length == 3){
+                    //get line data
+                    fromNode = tokens[0];
+                    toNode = tokens[1];
+                    linkState = tokens[2];
+                    //set up the elements of the link if needed
+                    fromNodeIndex = graphinfo.elements.indexOf(fromNode);
+                    toNodeIndex = graphinfo.elements.indexOf(toNode);
+                    //TODO: these missing nodes should probably be activated along with the link creation
+                    if(fromNodeIndex == -1){
+                        graphinfo.elements.push(fromNode)
+                        fromNodeIndex = graphinfo.elements.length-1;
+                    }
+                    if(toNodeIndex == -1){
+                        graphinfo.elements.push(toNode)
+                        toNodeIndex = graphinfo.elements.length-1;
+                    }
+                    //set up the link
+                    linkIndex = graphinfo.links.indexOf(fromNode+' '+toNode);
+                    if(linkIndex == -1){
+                        graphinfo.links.push(fromNode+' '+toNode);
+                        linkIndex = graphinfo.links.length-1;
+                    }
+                    state.linkStates.push({i: linkIndex,s: linkState});
+                }//end link parse
+            }//end change line parse
+        }//end change state parse
+    }//end graph state parse
     //clean up data
     for(i=0;i<graphinfo.states.length;i++){
-        state = graphinfo.states[i]; //shortcut
+        var state = graphinfo.states[i]; //shortcut
         for(j=0;j<graphinfo.elements.length;j++){
             if(!state.elementStates[j]){
                 state.elementStates[j] = 'nonactive';
@@ -149,7 +208,7 @@ function parseSimpleAdaptonView(text){
         graphinfo.elements[i] = makeElement(graphinfo.elements[i]);
     }
     for(i=0;i<graphinfo.links.length;i++){
-        tofrom = graphinfo.links[i].split(',');
+        var tofrom = graphinfo.links[i].split(' ');
         graphinfo.links[i] = makeLink(tofrom[0],tofrom[1]);
     }
     //return list of elements to graph
@@ -197,20 +256,26 @@ function handleFileSelect(event) {
         console.log('loading first states');
         graphinfo.currentBState = 0;
         graphinfo.currentCState = -1;
+        graphinfo.currentFullState = {
+            elementStates: [],
+            linkStates: []
+        }
         for(i=0;i<graphinfo.elements.length;i++){
             graphinfo.elements[i].addClass(graphinfo.states[0].elementStates[i]);
+            graphinfo.currentFullState.elementStates[i] = graphinfo.states[0].elementStates[i];
         }
         for(i=0;i<graphinfo.links.length;i++){
             graphinfo.links[i].addClass(graphinfo.states[0].linkStates[i]);
+            graphinfo.currentFullState.linkStates[i] = graphinfo.states[0].linkStates[i];
         }
         //populate list box with states
         for(i=0;i<graphinfo.states.length;i++){
+            $('#list').append('<option value="'+i+'c-1">'+graphinfo.states[i].title+'</option>');
             if(graphinfo.states[i].cstates && graphinfo.states[i].cstates.length){
                 for(c=0;c<graphinfo.states[i].cstates.length;c++){
-                    $('#list').append('<option value="'+i+'c+'+c+'+">'+graphinfo.states[i].title+'</option>');
+                    $('#list').append('<option value="'+i+'c'+c+'">-'+graphinfo.states[i].cstates[c].title+'</option>');
                 }
             }
-            $('#list').append('<option value="'+i+'c-1">'+graphinfo.states[i].title+'</option>');
         }
         console.log('ready for user interation');
     }//end file load handler
@@ -227,40 +292,56 @@ function handleListSelect(event){
 
     if(baseState == -1 || baseState == NaN) return;
     //shortcuts
-    var eso = graphinfo.states[graphinfo.currentBState].elementStates;
-    var lso = graphinfo.states[graphinfo.currentBState].linkStates;
+    var eso = graphinfo.currentFullState.elementStates;
+    var lso = graphinfo.currentFullState.linkStates;
     var esn = graphinfo.states[baseState].elementStates;
     var lsn = graphinfo.states[baseState].linkStates;
 
+    //forward within the same base state
     if(graphinfo.currentBState == baseState && graphinfo.currentCState < changeState){
         //only load the changes
         for(cs=graphinfo.currentCState+1;cs<=changeState;cs++){
             var ecs = graphinfo.states[baseState].cstates[cs].elementStates;
             var lcs = graphinfo.states[baseState].cstates[cs].linkStates;
-            graphinfo.elements[ecs.e].removeClass(ecs.o).addClass(ecs.n);
-            graphinfo.elements[lcs.e].removeClass(lcs.o).addClass(lcs.n);
+            for(c=0;c<ecs.length;c++){
+                graphinfo.elements[ecs[c].i].removeClass(eso[ecs[c].i]).addClass(ecs[c].s);
+                eso[ecs[c].i] = ecs[c].s;                
+            }
+            for(c=0;c<lcs.length;c++){
+                graphinfo.links[lcs[c].i].removeClass(lso[lcs[c].i]).addClass(lcs[c].s);
+                lso[lcs[c].i] = lcs[c].s;
+            }
         }
+    //arbitrary change of states
     }else{
         //change the state of all the stored objects to the base state
         for(i=0;i<graphinfo.elements.length;i++){
             if(esn[i] != eso[i]){
                 graphinfo.elements[i].removeClass(eso[i]).addClass(esn[i]);
+                eso[i] = esn[i];
             }
         }
         for(i=0;i<graphinfo.links.length;i++){
             if(lsn[i] != lso[i]){
                 graphinfo.links[i].removeClass(lso[i]).addClass(lsn[i]);
+                lso[i]=lsn[i];
             }
         }
         //add all the changes
         for(cs=0;cs<=changeState;cs++){
             var ecs = graphinfo.states[baseState].cstates[cs].elementStates;
             var lcs = graphinfo.states[baseState].cstates[cs].linkStates;
-            graphinfo.elements[ecs.e].removeClass(ecs.o).addClass(ecs.n);
-            graphinfo.elements[lcs.e].removeClass(lcs.o).addClass(lcs.n);
+            for(c=0;c<ecs.length;c++){
+                graphinfo.elements[ecs[c].i].removeClass(eso[ecs[c].i]).addClass(ecs[c].s);
+                eso[ecs[c].i] = ecs[c].s;
+            }
+            for(c=0;c<lcs.length;c++){
+                graphinfo.links[lcs[c].i].removeClass(lso[lcs[c].i]).addClass(lcs[c].s);
+                lso[lcs[c].i] = lcs[c].s;
+            }
         }    
     }
     //set current state
     graphinfo.currentBState = baseState;
-    graphinfo.currentcState = changeState;
+    graphinfo.currentCState = changeState;
 }
